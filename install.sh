05b8187e8b254795f75fe46690c0b2d2a449d8ea
szykol <szymonkolton@gmail.com>
2021-02-27 10:58:33 +0100

    Use home variable

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ echo "Installing syntax highlighting for oh-my-zsh"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 echo "Installing fzf"
-git clone --depth 0 https://github.com/junegunn/fzf.git ~/.fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
